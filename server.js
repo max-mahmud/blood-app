9c686fecf0f073e80a05c00fe08fff76ffd48ab6
max-mahmud <newmahmud24@gmail.com>
2023-07-04 08:46:02 +0600

    vercel add

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 
-
 //dot config
 dotenv.config();
 connectDB();
 
-
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", " POST", " PUT", " DELETE"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 
 //routes
